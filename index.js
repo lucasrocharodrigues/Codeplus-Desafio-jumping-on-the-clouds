f8d8f2e68dcd58b7a47d76908a8de6edfa6701c8
lucasrocharodrigues <lucasrocha10111997@gmail.com>
2020-12-07 21:40:43 -0300

    finalização

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const cenario1 = [0, 0, 1, 0, 0, 1, 0];
 const cenario2 = [0, 0, 0, 0, 1, 0];
 
 const jumpingOnClouds = c => {
-
+    let jump = 0;
+    for (let cloud = 0; cloud < c.length; cloud++) {
+        if (c[cloud + 1] != undefined) jump++
+        if (c[cloud + 2] === 0) cloud++
+    }
+    return jump
 }
 
-jumpingOnClouds(cenario1)
+console.log(jumpingOnClouds(cenario2))
 
-//c representa o cenario, mantenha o nome pra eu poder testar no hackerrank :D
+//c representa o cenario, mantenha o nome pra eu poder testar no hackerrank :Dj
